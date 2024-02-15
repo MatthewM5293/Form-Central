ebe328354ccca2a1d83581662e0aac5e1ebfdbcb
MatthewM5293 <mmunozramirez@student.neumont.edu>
2024-02-14 21:16:48 -0700

    Base for home page is setup

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,73 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      //app name
+      title: "Form Central",
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Row(
+            children: [
+              Image(
+                  image: NetworkImage(
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png"),
+                  height: 45,
+                  width: 45),
+              Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+              Text("Form Central"),
+            ],
+          ),
+          centerTitle: true,
         ),
+        body: const Card(
+          color: Colors.deepOrange,
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+                Image(
+                    image: NetworkImage(
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png"),
+                    height: 20,
+                    width: 20),
+                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+                Text(
+                  "Temp",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0)),
+                Text(
+                    "Desc will go here but for now it will just be Hello World!")
+              ],
+            )
+          ]),
+        ),
+        bottomNavigationBar: BottomNavigationBar(currentIndex: 0, items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: "Home",
+              backgroundColor: Colors.deepOrange),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search",
+              backgroundColor: Colors.deepOrange),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: "Create",
+              backgroundColor: Colors.deepOrange),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: "Notifications"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), label: "Profile"),
+        ]),
       ),
     );
   }
